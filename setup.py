15686957bee890f796abec2d77bad3555f4b670c
benjaminesser <bencesser@gmail.com>
2024-05-31 21:10:33 -0700

    Merged branch 'ben' into 'main' with all changes from 'ben'

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='pathofinder',
+    version='1.0',
+    description='Command-line script to perform variant calling of mpileup files',
+    author='Benjamin Esser and Ehsun Yazdani',
+    author_email='besser@ucsd.edu and eyazdani@ucsd.edu',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "pathofinder=pathofinder.pathofinder:main"
+        ],
+    },
+)
